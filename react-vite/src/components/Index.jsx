57ef27b18df1bb1152cb017ae6702ab8efe32c23
aileks <aeverly14@pm.me>
2024-09-29 20:31:47 -0400

    tailwind init

---
+++ react-vite/src/components/Index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <div>
+      <h1 className='text-3xl text-center font-bold'>Hello World!</h1>
+      <h2 className='text-2xl text-center font-semibold'>Hello World!</h2>
+      <h3 className='text-xl text-center font-medium'>Hello World!</h3>
+      <h4 className='text-lg text-center'>Hello World!</h4>
+    </div>
+  );
+}
