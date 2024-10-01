db8dd4992261c7c2810fa149b82e0b53b879c369
aileks <aeverly14@pm.me>
2024-10-01 18:34:53 -0400

    added logo and favicon

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -5,10 +5,16 @@ export default function Navigation() {
   return (
     <nav className='border-b border-blue-500 bg-white shadow-md py-4 px-4'>
       <ul className='flex items-center justify-between'>
-        <li>
+        <li className='flex items-center'>
+          <img
+            src='/brain_logo.jpg'
+            className='h-10 w-10 rounded-full'
+            alt='logo image'
+          />
+
           <NavLink
             to='/'
-            className='text-blue-600 hover:text-blue-400 font-medium transition duration-200'
+            className='ml-2 text-blue-600 hover:text-blue-400 font-medium transition duration-200'
           >
             Home
           </NavLink>
